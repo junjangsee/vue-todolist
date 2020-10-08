2bb4000a6491fb2d7607c63be3d9e769c7445ff0
junjang <junjang.see@gmail.com>
2020-10-08 17:37:22 +0900

    [#9]feat: Todo 추가기능 구현

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -1,9 +1,31 @@
 <template>
-  <input placeholder="Write Your Todo..." />
+  <input
+    type="text"
+    v-model="text"
+    class="new-todo"
+    placeholder="Write Your Todo..."
+    autofocus
+    autocomplete="off"
+    v-on:keypress.enter="handleInput"
+  />
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      text: "",
+    };
+  },
+  methods: {
+    handleInput() {
+      if (this.text === "") return;
+
+      this.$emit("onAddTodo", this.text);
+      this.text = "";
+    },
+  },
+};
 </script>
 
 <style scoped>
