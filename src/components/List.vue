6ec2e5928d77d92dec3c2429ac643c28a89d8df5
junjang <junjang.see@gmail.com>
2020-10-08 16:07:06 +0900

    [#7]feat: List 컴포넌트를 추가합니다.

---
+++ src/components/List.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="main">
+    <ul class="todo-list">
+      <li>todo</li>
+      <!-- <li></li> ...  -->
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
