4ecd1a60a6ae8464ae3ab03b34829be5b0f3c147
junjang <junjang.see@gmail.com>
2020-10-09 20:31:00 +0900

    [#19]feat: 할 일 필터링 구현

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,15 +1,40 @@
 <template>
   <footer class="footer">
+    <span class="todo-count">
+      <strong>{{ size }}</strong> items left
+    </span>
     <ul class="filter-list">
-      <!-- <li>
-            <button></button>
-          </li> ... -->
+      <li v-for="(filter, idx) in filters" v-bind:key="idx">
+        <button
+          v-bind:class="{selected: filterType === filter}"
+          @click="handleFilterType(filter)"
+          type="button"
+        >
+          {{ filter }}
+        </button>
+      </li>
+      <li>
+        <button class="clear-todo" type="button">Clear Todo</button>
+      </li>
     </ul>
   </footer>
 </template>
 
 <script>
-export default {};
+export default {
+  props: ["filterType", "size"],
+  data() {
+    return {
+      filters: ["ALL", "DOING", "DONE"],
+    };
+  },
+
+  methods: {
+    handleFilterType(type) {
+      this.$emit("onFilterType", type);
+    },
+  },
+};
 </script>
 
 <style scoped></style>
