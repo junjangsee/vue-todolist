51b0622e6c92a059341a2613b11b47b8d2e710cb
junjang <junjang.see@gmail.com>
2020-10-09 22:55:26 +0900

    [#25]style: 할 일 컨테이너 스타일링

--- src/App.vue
+++ src/App.vue
@@ -118,10 +118,19 @@ export default {
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #56bdfb;
+}
+
+.todo-wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 400px;
+  height: 600px;
 }
 </style>
