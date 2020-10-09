b3d54d3ccc141781e3a3e6b58f425db3a57c6057
junjang <junjang.see@gmail.com>
2020-10-09 16:41:17 +0900

    [#17]feat: Footer 컴포넌트 구현

---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer class="footer">
+    <ul class="filter-list">
+      <!-- <li>
+            <button></button>
+          </li> ... -->
+    </ul>
+  </footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
