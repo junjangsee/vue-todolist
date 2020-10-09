b83bc20c8f2128bc567fdcfcfd5306a9c8c25995
junjang <junjang.see@gmail.com>
2020-10-09 23:09:57 +0900

    [#27]style: Header 텍스트 스타일 변경

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,4 +7,13 @@ export default {};
 </script>
 
 <style scoped>
+.header {
+  width: 100%;
+  margin: 0 0 30px 0;
+  text-align: center;
+  font-size: 30px;
+  font-weight: 600;
+  color: #fff;
+  letter-spacing: 8px;
+}
 </style>
